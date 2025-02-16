adab427fed479e2bf4b1e669ff72c78f84326ba7
Zekicks <dimagamerakk@gmail.com>
2025-02-16 06:24:52 +0000

    Update 2 files
    - /README.md
    - /next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-let userConfig = undefined;
+let userConfig = undefined
 try {
-  userConfig = await import('./v0-user-next.config');
+  userConfig = await import('./v0-user-next.config')
 } catch (e) {
-  // игнорируем ошибку, если файл не существует
+  // ignore error
 }
 
 /** @type {import('next').NextConfig} */
@@ -16,18 +16,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'export', // Добавляем этот параметр для статического экспорта
   experimental: {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-};
+}
+
+mergeConfig(nextConfig, userConfig)
 
 function mergeConfig(nextConfig, userConfig) {
   if (!userConfig) {
-    return;
+    return
   }
+
   for (const key in userConfig) {
     if (
       typeof nextConfig[key] === 'object' &&
@@ -36,13 +38,11 @@ function mergeConfig(nextConfig, userConfig) {
       nextConfig[key] = {
         ...nextConfig[key],
         ...userConfig[key],
-      };
+      }
     } else {
-      nextConfig[key] = userConfig[key];
+      nextConfig[key] = userConfig[key]
     }
   }
 }
 
-mergeConfig(nextConfig, userConfig);
-
-export default nextConfig;
+export default nextConfig
